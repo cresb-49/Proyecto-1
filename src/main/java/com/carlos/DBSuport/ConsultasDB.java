b3fe7f8d607e37aea7fb720312d5f1be4fec5a77
cresb-49 <66991258+cresb-49@users.noreply.github.com>
2020-08-11 11:56:04 -0600

    Lineas de conexion a base de datos

--- src/main/java/com/carlos/DBSuport/ConsultasDB.java
+++ src/main/java/com/carlos/DBSuport/ConsultasDB.java
@@ -1,7 +1,18 @@
 package com.carlos.DBSuport;
 
+import java.sql.*;
+
 public class ConsultasDB {
+    ConexionDB conexiondb = new ConexionDB();
+    Connection conexion;
+    ResultSet resultado;
+    /**
+     * CONSTRUCTOR VACIO DE LA CLASE
+     */
     public ConsultasDB(){
+        
+    }
+    public void consultaClientes(){
         
     }
 }
