f921810bb1767e57fb836d90934145c36d5189c7
cresb-49 <66991258+cresb-49@users.noreply.github.com>
2020-08-23 02:57:40 -0600

    Arrrglo de documentacion interna del programa

--- Script database/GenerarUsuario.sql
+++
@@ -1,3 +0,0 @@
-CREATE USER IF NOT EXISTS 'carlospac'@'localhost' IDENTIFIED BY 'Carlos_Pac_201931012';
-USE mi_empresa_proyecto1;
-GRANT ALL PRIVILEGES ON mi_empresa_proyecto1.* TO 'carlospac'@'localhost';
